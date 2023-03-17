5b50f375ee5788fae9dfd3c894f60f54712b5483
damiansarnecki <xdamiansarnecki@gmail.com>
2023-03-17 21:36:49 +0100

    save

--- src/App.js
+++ src/App.js
@@ -22,15 +22,15 @@ function App() {
         <Grid alignItems="center" minH="100vh" p={3}>
           <VStack spacing={8}>
             <Image mr="15px" src="/mystra-logo.svg" minW="300px" maxW="300px" />
-            <Box maxW="500px"> Our landing page is currently being rebranded. In the meantime, please visit the old landing page </Box>
+            <Box maxW="500px" color="white"> Our landing page is currently being rebranded. In the meantime, please visit the old landing page </Box>
             <Flex gap="12px" flexDir={{base: "column", md: "initial"}}>
               <Link href="https://casperarmy.org/tickets" >
-                <Button _hover={{textDecoration:"none", bgColor: "#964BF7" }} bg="#964BF7">
+                <Button color="white" _hover={{textDecoration:"none", bgColor: "#964BF7" }} bg="#964BF7">
                   Buy Call-up Ticket!
                 </Button>
               </Link>
               <Link href="https://casperarmy.org/">
-                <Button _hover={{textDecoration:"none" , bgColor: "#964BF7" }} bg="none" border="1px solid" borderColor="#964BF7" _hover={{bg: "#964BF7"}}>
+                <Button color="white" _hover={{textDecoration:"none" , bgColor: "#964BF7" }} bg="none" border="1px solid" borderColor="#964BF7" _hover={{bg: "#964BF7"}}>
                   Visit old landing page
                 </Button>
               </Link>
