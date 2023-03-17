69598f726b7ec014a95047f3d5faf5f909d7f479
damiansarnecki <xdamiansarnecki@gmail.com>
2023-03-17 18:56:25 +0100

    save

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   Code,
   Grid,
   theme,
+  Image,
+  Button,
+  Flex,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -15,23 +18,23 @@ import { Logo } from './Logo';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box textAlign="center" fontSize="xl" bg="#111111">
+        <Grid alignItems="center" minH="100vh" p={3}>
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Image mr="15px" src="/mystra-logo.svg" minW="300px" maxW="300px" />
+            <Box maxW="500px"> Our landing page is currently being rebranded. In the meantime, please visit the old landing page </Box>
+            <Flex gap="12px" flexDir={{base: "column", md: "initial"}}>
+              <Link href="https://casperarmy.org/tickets" >
+                <Button _hover={{textDecoration:"none", bgColor: "#964BF7" }} bg="#964BF7">
+                  Buy Call-up Ticket!
+                </Button>
+              </Link>
+              <Link href="https://casperarmy.org/">
+                <Button _hover={{textDecoration:"none" , bgColor: "#964BF7" }} bg="none" border="1px solid" borderColor="#964BF7" _hover={{bg: "#964BF7"}}>
+                  Visit old landing page
+                </Button>
+              </Link>
+            </Flex>
           </VStack>
         </Grid>
       </Box>
