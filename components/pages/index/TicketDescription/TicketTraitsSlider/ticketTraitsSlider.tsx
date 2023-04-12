306bef3485d04e00979e02ee3b2caac7a5724e9e
damiansarnecki <xdamiansarnecki@gmail.com>
2023-04-12 12:50:51 +0200

    fixed slider component

--- components/pages/index/TicketDescription/TicketTraitsSlider/ticketTraitsSlider.tsx
+++ components/pages/index/TicketDescription/TicketTraitsSlider/ticketTraitsSlider.tsx
@@ -1,5 +1,7 @@
 import { Box, Flex, Grid } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const SLIDER_CHANGE_TIME = 30;
 
 export const TicketTraitsSlider = ({
   traits,
@@ -7,9 +9,47 @@ export const TicketTraitsSlider = ({
   traits: { label: string; content: any }[];
 }) => {
   const [currentTrait, setCurrentTrait] = useState<number>(0);
+  const [stopped, setStopped] = useState<boolean>(false);
+
+  const increaseCounter = () => {
+    setCurrentTrait((prev) => {
+      return prev == traits.length - 1 ? 0 : prev + 1;
+    });
+  };
+
+  const [time, setTime] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(time);
+    if (time == SLIDER_CHANGE_TIME) {
+      setTime(0);
+      increaseCounter();
+    }
+  }, [time]);
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      if (!stopped) {
+        setTime((prev) => {
+          return prev + 1;
+        });
+      }
+    }, 100);
+    return () => clearInterval(timer);
+  }, [stopped]);
 
   return (
-    <Flex flexDir="column" mt={{base: '30px', md: "100px"}} maxW='420px'>
+    <Flex
+      onMouseEnter={() => {
+        setStopped(true);
+      }}
+      onMouseLeave={() => {
+        setStopped(false);
+      }}
+      flexDir="column"
+      mt={{ base: "30px", md: "100px" }}
+      maxW="420px"
+    >
       <Box fontSize="16px" fontFamily="Sora" fontWeight="700">
         {"0" + (currentTrait + 1).toString()}
       </Box>
@@ -26,20 +66,44 @@ export const TicketTraitsSlider = ({
       <Box
         mt="20px"
         fontWeight="400"
-
         fontSize="14px"
         fontFamily="Sora"
-		lineHeight="160%"
+        lineHeight="160%"
         color="#CEE5F2"
-		
       >
         {traits.length > 0 ? traits[currentTrait].content : "Loading"}
       </Box>
-	  <Grid gap="8px" mt="25px" templateColumns={`repeat(${traits.length}, 1fr)`}>
-	  {traits.map((item, index) => {
-		  return <Box key={item.label} cursor="pointer" onClick={() => setCurrentTrait(index)} h="8px" bg={currentTrait == index ? "#04D7B1" : "none"} border="1px solid" borderColor="#04D7B1" borderRadius="2px"/>
-	  })}
-	  </Grid>
+      <Grid
+        gap="8px"
+        mt="25px"
+        templateColumns={`repeat(${traits.length}, 1fr)`}
+      >
+        {traits.map((item, index) => {
+          return (
+            <Box
+              key={item.label}
+              cursor="pointer"
+              onClick={() => {
+                setCurrentTrait(index);
+                setTime(0);
+              }}
+              h="8px"
+              border="1px solid"
+              pos="relative"
+              borderColor={currentTrait == index ? "#04D7B1" : "gray"}
+              borderRadius="2px"
+            >
+              <Box
+                bg={currentTrait == index ? "#04D7B1" : "none"}
+                w={`${
+                  ((time % SLIDER_CHANGE_TIME) / SLIDER_CHANGE_TIME) * 100
+                }%`}
+                h="100%"
+              ></Box>
+            </Box>
+          );
+        })}
+      </Grid>
     </Flex>
   );
 };
