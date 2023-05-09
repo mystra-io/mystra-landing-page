fe52149dd4c75cf471d9e189f76edaf041a6954d
mystra.io <125320652+mystraio@users.noreply.github.com>
2023-05-09 17:11:52 +0200

    Update items.tsx

--- components/pages/investor/InfoWithItems/items.tsx
+++ components/pages/investor/InfoWithItems/items.tsx
@@ -4,11 +4,11 @@ export const PUBLIC_INVEST_ITEMS = {
 		"Minted nft tickets will be sent to the investor's wallet within 48 hours.",
 		"Tickets will be minted and sent to the addresses of the wallets from which the mint was made.",
 		"The investor is not charged for the minting cost on Casper Network",
-		"It is possible to burn tickets to get a $MYSTRA tokens (after TGE + vesting) or sell tickets on NFT marketplace (any time after tickets received - no vesting). We guarantee that after the Public Sale Round 5 is over, users will be publicly informed that platform tickets can be acquired on the secondary market. ",
+		"It is possible to burn tickets to get a $MYSTRA tokens (after TGE + vesting) or sell tickets on NFT marketplace (any time after tickets received - no vesting). We guarantee that after the Public Mint Round 5 is over, users will be publicly informed that platform tickets can be acquired on the secondary market. ",
 		"We guarantee free promotion of tickets issued by private and Venture investors on our marketplace. Promotion will take place directly on Mystra marketplace, mystra.io, on the Mystra platform mystra.app, and on all Mystra social media channels, i.e. Twitter, Telegram, Discord, Reddit, Medium, and through our partners."
 	],
 	about: [
-		"$MYSTRA tokens can only be minted and claimed by burning Mystra Tickets. The more tickets you mint, the more $MYSTRA tokens you can mint in the seed, private, and public rounds. The allocation of $MYSTRA tokens is equal for each round of ticket sales",
+		"$MYSTRA tokens can only be minted and claimed by burning Mystra Tickets. The more tickets you mint, the more $MYSTRA tokens you can mint in the seed, private, and public rounds. The allocation of $MYSTRA tokens is equal for each round of ticket mints",
 		"Unlock both CEXes and DEX launches (therefore stronger price action)",
 		"$MYSTRA token staking with dynamic & sustainable APY rewards.",
 		"Up to 25% revenues dedicated to token burning from Mystra platform revenue model and up to 25% to $MYSTRA liquid pool, and up to 50% of dedicated revenues goes to Mystra treasury managed by DAO. More information about revenues is available in the pitchdeck.",
